43d416178654dfb6699cb21b65da960f062c45d2
consiglieri-cfi <30444483+consiglieri-cfi@users.noreply.github.com>
2021-06-20 10:36:10 -0500

    Update syncMonitor.sh

--- syncMonitor/syncMonitor.sh
+++ syncMonitor/syncMonitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#  algodMon - syncMonitor - v1.0 - Node Synchronization Monitor
+#  algodMon - syncMonitor - v1.1 - Node Synchronization Monitor
 # 
 #  Donations: OBQIVIPTUXZENH2YH3C63RHOGS7SUGGQTNJ52JR6YFHEVFK5BR7BEYKQKI
 #
