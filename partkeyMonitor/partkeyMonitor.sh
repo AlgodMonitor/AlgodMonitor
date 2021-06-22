83b70e2b6e29050709e88c778f8c4c4969377b8c
consiglieri-cfi <30444483+consiglieri-cfi@users.noreply.github.com>
2021-06-22 05:07:36 -0500

    Update partkeyMonitor.sh

--- partkeyMonitor/partkeyMonitor.sh
+++ partkeyMonitor/partkeyMonitor.sh
@@ -18,14 +18,17 @@ export ALGORAND_DATA=${dataDir};
 echo -e "\n\n${brk}\nalgodMon - partkeyMonitor - Participation Key Expiration - Initialization\n${brk}";
 
 # Execution Tracker
-echo -e "\n\nLast Executed: $(date -r ${logDir}/monitorKeyExpiration.log +"%Y-%m-%d %H:%M:%S" 2>/dev/null)\nCurrent Time:  ${currentDate} ${currentSecond}\n"
+echo -e "\n\nLast Executed: $(date -r ${logDir}/monitorExpiration.log +"%Y-%m-%d %H:%M:%S" 2>/dev/null)\nCurrent Time:  ${currentDate} ${currentSecond}\n"
 
 # Check round
-currentRound=$(~/node/goal node status | grep "Last committed" | awk '{print $NF}');
-endRound=$(~/node/goal account listpartkeys | tail -n1 | awk '{print $5}');
+currentRound=$(${nodeDir}/goal node status | grep "Last committed" | awk '{print $NF}');
+keyState=$(${nodeDir}/goal account listpartkeys | tail -n1 | awk '{print $1, $5}');
+registerState=$(echo ${keyState} | awk '{print $1}');
+lastRound=$(echo ${keyState} | awk '{print $2}');
+
 
 # Calculate expiration
-deltaRound=$(expr ${endRound} - ${currentRound});
+deltaRound=$(expr ${lastRound} - ${currentRound});
 deltaSeconds=$(expr ${deltaRound} \* 4);
 deltaEpoch=$(expr ${currentEpoch} + ${deltaSeconds});
 deltaDays=$(expr ${deltaSeconds} / 86400);
@@ -35,13 +38,13 @@ monitorKeyExpiration=${deltaDays};
 echo -e "monitorKeyExpiration=${monitorKeyExpiration}" > ${logDir}/monitorAlertExpire.log;
 
 # Update Report
-echo -e "${currentTime} ${currentRound} ${endRound} ${deltaRound} ${deltaDays} $(date -d @${deltaEpoch} +%Y-%m-%d-%H:%M:%S)" >> ${logDir}/monitorExpiration.log;
+echo -e "${currentTime} ${currentRound} ${lastRound} ${deltaRound} ${deltaDays} $(date -d @${deltaEpoch} +%Y-%m-%d-%H:%M:%S) ${registerState}" >> ${logDir}/monitorExpiration.log;
 
 # Show Summary
 echo -e "\nParticipation Key is valid for ${monitorKeyExpiration} days.";
 
 # Show Report
 echo -e "\n\n${brk}\nalgodMon - partkeyMonitor - Participation Key Expiration - Report\n${brk}\n";
-echo -e "Date Time Current End Rounds Days Expiration\n$(tail -n 20 ${logDir}/monitorExpiration.log)" | column -t
+echo -e "Date Time Current End RoundsLeft DaysLeft Expiration Registered\n$(tail -n 20 ${logDir}/monitorExpiration.log)" | column -t
 
 #EOF
