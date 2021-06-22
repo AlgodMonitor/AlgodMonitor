2ac3e813d8869d9cdf08cecd30799bf2b1992bbc
consiglieri-cfi <30444483+consiglieri-cfi@users.noreply.github.com>
2021-06-22 01:25:05 -0500

    Update networkMonitor.sh

--- networkMonitor/networkMonitor.sh
+++ networkMonitor/networkMonitor.sh
@@ -11,6 +11,8 @@
 #
 bk=$(printf '=%.0s' {0..50}); brk=$(printf '=%.0s' {1..60}); brks=$(printf '=%.0s' {1..30});
 sourceDir=$(dirname "$0");
+logDir=${sourceDir}/../logs;
+configDir=${sourceDir}/../config;
 currentEpoch=$(date +%s);
 export ALGORAND_DATA=~/node/data;
 nodePath=$(echo ${ALGORAND_DATA} | sed 's/\/node\/data/\/node/g');
@@ -19,12 +21,11 @@ nodePath=$(echo ${ALGORAND_DATA} | sed 's/\/node\/data/\/node/g');
 echo -e "\n\n${brk}\nalgodMon - networkMonitor - Network Monitor Report\n${brk}";
 echo -e "\n${bk}\nNetwork Peers\n${bk}\n";
 
-# Process netstat
-netstat -natp 2>/dev/null > ${sourceDir}/netstat-${currentEpoch}
-grep algod ${sourceDir}/netstat-${currentEpoch} | awk '{print $5}' > ${sourceDir}/peersCurrent.log
+# Peer Connections - 'lsof -i'
+timeout 10 lsof -i :4160 | awk '{print $(NF-1)}' | awk -F"->" '{print $2}' | awk -F":" '{print $1}' | awk 'NF' > ${logDir}/lsof-4160-${currentEpoch}
 
-# Lookup peers
-while read ipAddr; do nslookup $(echo $ipAddr | awk -F: '{print $1}'); done < ${sourceDir}/peersCurrent.log | grep "name =" | tee -a ${sourceDir}/peersCurrentLookup.log
+# Peer Connections - summary
+cat $(find ${logDir}/ -name 'lsof-4160*' -mtime -7) | sort | uniq -c > ${logDir}/monitorPeers
 
-# Remove netstat
-rm -f ${sourceDir}/netstat-${currentEpoch}
+# Remove 'lsof -i'
+rm -f ${logDir}/lsof-4160-${currentEpoch}
