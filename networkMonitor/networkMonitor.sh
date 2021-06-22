249effe2997e5f545e36f30da0e16ec9a50cb01c
consiglieri-cfi <30444483+consiglieri-cfi@users.noreply.github.com>
2021-06-22 05:12:45 -0500

    Update networkMonitor.sh

--- networkMonitor/networkMonitor.sh
+++ networkMonitor/networkMonitor.sh
@@ -16,7 +16,7 @@ export ALGORAND_DATA=${dataDir};
 
 # Banner
 echo -e "\n\n${brk}\nalgodMon - networkMonitor - Network Monitor Report\n${brk}";
-echo -e "\n${bk}\nNetwork Peers\n${bk}\n";
+echo -e "\n${brk}\nNetwork Peers\n${brk}\n";
 
 # Peer Connections - 'lsof -i'
 timeout 10 lsof -i :4160 | awk '{print $(NF-1)}' | awk -F"->" '{print $2}' | awk -F":" '{print $1}' | awk 'NF' > ${logDir}/networkPeers/lsof-4160-${currentEpoch}
@@ -25,4 +25,5 @@ timeout 10 lsof -i :4160 | awk '{print $(NF-1)}' | awk -F"->" '{print $2}' | awk
 cat $(find ${logDir}/networkPeers -name 'lsof-4160*' -mtime -7) | sort | uniq -c | tee ${logDir}/networkPeers/monitorPeers-{currentEpoch}
 
 # Remove 'lsof -i'
+echo -e "\n\nRemoving temporary files:  ${logDir}/lsof-4160-${currentEpoch}\n\n"
 rm -f ${logDir}/lsof-4160-${currentEpoch}
