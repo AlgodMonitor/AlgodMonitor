2544afa6745b39ed24ac4e80445eb717a4ac0622
consiglieri-cfi <30444483+consiglieri-cfi@users.noreply.github.com>
2021-06-22 01:53:15 -0500

    Update tokenMonitor.sh

--- tokenMonitor/tokenMonitor.sh
+++ tokenMonitor/tokenMonitor.sh
@@ -10,8 +10,9 @@
 # Initialization
 export ALGORAND_DATA=~/node/data;
 nodeDir=~/node;
-nodePath=~/node;
 sourceDir=$(dirname "$0");
+logDir=${sourceDir}/../logs;
+configDir=${sourceDir}/../config;
 currentDate=$(date +%Y-%m-%d);
 currentSecond=$(date +%H:%M:%S);
 currentEpoch=$(date +%s);
@@ -20,7 +21,7 @@ brk=$(printf '=%.0s' {1..120}); brkm=$(printf '=%.0s' {1..70}); brks=$(printf '=
 echo -e "\n\n${brk}\nalgodMon - tokenMonitor - Token Report - ${currentDate}\n${brk}";
 
 # Configuration - Add Wallet
-if [ ! -f ${sourceDir}/walletAddress.cfg ]; then
+if [ ! -f ${configDir}/walletAddress.cfg ]; then
 echo -e "\n${brkm}\nConfiguration\n${brkm}\n";
 echo -e "You have not added monitoring for any public wallet addresses.\n\nDo you want to add any wallets to monitoring? (y/n)\n\n";
 read configMonitor;
@@ -30,7 +31,7 @@ echo -e "\n\nEnter a name for the account...\n\n"; read monitorName;
 echo -e "\n\nYou entered the following address:\n\n\t${monitorWallet}\n\t${monitorName}\n\n\nIs this correct? (y/n)\n\n";
 read validateAddress;
 if [[ ${validateAddress} == "y" ]]; then
-echo "${monitorName} ${monitorWallet}" >> ${sourceDir}/walletAddress.cfg;
+echo "${monitorName} ${monitorWallet}" >> ${configDir}/walletAddress.cfg;
 fi; fi; fi;
 
 # Supply Monitor
@@ -38,23 +39,26 @@ echo -e "\n${brkm}\nSupply Metrics\n${brkm}\n";
 ${nodeDir}/goal ledger supply;
 
 # Supply History
-echo -e "\n${brkm}\nSupply History\n${brkm}\n"; echo -e "${currentTime} \t $(${nodeDir}/goal ledger supply | grep Total)" >> ${sourceDir}/supplyMonitor.log;
-head -n1 ${sourceDir}/supplyMonitor.log;
-tail ${sourceDir}/supplyMonitor.log;
+currentSupply=$(${nodeDir}/goal ledger supply | grep Total);
+echo -e "\n${brkm}\nSupply History\n${brkm}\n"; echo -e "${currentTime} \t ${currentSupply}" >> ${logDir}/monitorSupply.log;
+head -n1 ${logDir}/monitorSupply.log;
+tail ${logDir}/monitorSupply.log;
 
 # Stake History
-echo -e "\n${brkm}\nOnline Stake\n${brkm}\n"; echo -e "${currentTime} \t $(${nodeDir}/goal ledger supply | grep -i online)" >> ${sourceDir}/onlineMonitor.log;
-head -n1 ${sourceDir}/onlineMonitor.log;
-tail ${sourceDir}/onlineMonitor.log;
+currentOnlineStake=$(${nodeDir}/goal ledger supply | grep Online);
+echo -e "\n${brkm}\nOnline Stake\n${brkm}\n"; echo -e "${currentTime} \t ${currentOnlineStake}" >> ${logDir}/monitorOnlineStake.log;
+head -n1 ${logDir}/monitorOnlineStake.log;
+tail ${logDir}/monitorOnlineStake.log;
 
 # Account Monitor
-if [ -f ${sourceDir}/walletAddress.cfg ]; then
-echo -e "\n\n${brk}\nalgodMon - tokenMonitor - Account Monitor - ${currentDate}\n${brk}";
+if [ -f ${configDir}/walletAddress.cfg ]; then
+echo -e "\n\n\n${brk}\nalgodMon - tokenMonitor - Account Monitor - ${currentDate}\n${brk}";
 while read walletName walletAddress; do
 	walletPrefix=${walletName}-$(echo ${walletAddress} | cut -c1-7);
+	walletBalance=$(${nodeDir}/goal account balance -a ${walletAddress});
 	echo -e "\n${brkm}\n${walletName}\n${brkm}\n";
-	echo -e "${currentTime} \t $(${nodeDir}/goal account balance -a ${walletAddress})" >> ${sourceDir}/walletBalance-${walletPrefix};
-	head -n 1 ${sourceDir}/walletBalance-${walletPrefix};
-	tail ${sourceDir}/walletBalance-${walletPrefix};
-done < ${sourceDir}/walletAddress.cfg
+	echo -e "${currentTime} \t ${walletBalance}" >> ${sourceDir}/walletBalance-${walletPrefix};
+	head -n 1 ${logDir}/walletBalance-${walletPrefix};
+	tail ${logDir}/walletBalance-${walletPrefix};
+done < ${configDir}/walletAddress.cfg
 fi;
