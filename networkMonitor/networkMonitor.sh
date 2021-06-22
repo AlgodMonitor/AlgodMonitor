a7bb0de6dcb0b2f5300e46235fd53b155ab69393
consiglieri-cfi <30444483+consiglieri-cfi@users.noreply.github.com>
2021-06-22 01:40:40 -0500

    Update networkMonitor.sh

--- networkMonitor/networkMonitor.sh
+++ networkMonitor/networkMonitor.sh
@@ -9,11 +9,15 @@
 
 # Set variables
 #
-bk=$(printf '=%.0s' {0..50}); brk=$(printf '=%.0s' {1..60}); brks=$(printf '=%.0s' {1..30});
+nodeDir=~/node;
 sourceDir=$(dirname "$0");
 logDir=${sourceDir}/../logs;
 configDir=${sourceDir}/../config;
+currentDate=$(date +%Y-%m-%d);
+currentSecond=$(date +%H:%M:%S);
 currentEpoch=$(date +%s);
+currentTime=$(echo -e "${currentDate}  ${currentSecond}");
+brk=$(printf '=%.0s' {1..120}); brkm=$(printf '=%.0s' {1..70}); brks=$(printf '=%.0s' {1..30});
 export ALGORAND_DATA=~/node/data;
 nodePath=$(echo ${ALGORAND_DATA} | sed 's/\/node\/data/\/node/g');
 
@@ -22,10 +26,10 @@ echo -e "\n\n${brk}\nalgodMon - networkMonitor - Network Monitor Report\n${brk}"
 echo -e "\n${bk}\nNetwork Peers\n${bk}\n";
 
 # Peer Connections - 'lsof -i'
-timeout 10 lsof -i :4160 | awk '{print $(NF-1)}' | awk -F"->" '{print $2}' | awk -F":" '{print $1}' | awk 'NF' > ${logDir}/lsof-4160-${currentEpoch}
+timeout 10 lsof -i :4160 | awk '{print $(NF-1)}' | awk -F"->" '{print $2}' | awk -F":" '{print $1}' | awk 'NF' > ${logDir}/networkPeers/lsof-4160-${currentEpoch}
 
 # Peer Connections - summary
-cat $(find ${logDir}/ -name 'lsof-4160*' -mtime -7) | sort | uniq -c > ${logDir}/monitorPeers
+cat $(find ${logDir}/networkPeers -name 'lsof-4160*' -mtime -7) | sort | uniq -c | tee ${logDir}/networkPeers/monitorPeers-{currentEpoch}
 
 # Remove 'lsof -i'
 rm -f ${logDir}/lsof-4160-${currentEpoch}
