168d1fa99cce03cb41fb0dc3bde33cb9be8c52ea
consiglieri-cfi <30444483+consiglieri-cfi@users.noreply.github.com>
2021-06-21 23:53:49 -0500

    Update syncMonitor.sh

--- syncMonitor/syncMonitor.sh
+++ syncMonitor/syncMonitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#  algodMon v1.1 - syncMonitor - Node Synchronization Monitor
-# 
+#  algodMon - v1.1 - syncMonitor - Node Synchronization Monitor
+#
 #  Donations: OBQIVIPTUXZENH2YH3C63RHOGS7SUGGQTNJ52JR6YFHEVFK5BR7BEYKQKI
 #
 #  Copyright 2021 - Consiglieri-cfi
@@ -9,6 +9,8 @@
 
 # Initialization
 sourceDir=$(dirname "$0");
+logDir=${sourceDir}/../logs;
+configDir=${sourceDir}/../config;
 currentDate=$(date +%Y-%m-%d);
 currentSecond=$(date +%H:%M:%S);
 currentEpoch=$(date +%s);
@@ -17,14 +19,14 @@ brk=$(printf '=%.0s' {1..120}); brkm=$(printf '=%.0s' {1..70}); brks=$(printf '=
 echo -e "\n\n${brk}\nalgodMon - syncMonitor - Node Synchronization Monitor\n${brk}";
 
 # Configuration - Data Directory
-if [[ ! -f ${sourceDir}/monitorConfig.cfg ]]; then
+if [[ ! -f ${configDir}/monitorConfig.cfg ]]; then
 	echo -e "\n\n${brks}\nConfiguration - Algorand Data\n${brks}";
 	echo -e "\nPlease specify the path to the ALGORAND_DATA directory...\n\nExample: $HOME/node/data\n\n"; read ALGORAND_DATA;
 	echo -e "\n\nYou have entered: ${ALGORAND_DATA}\n"
 	echo -e "\n${brks}\nSaving Config\n${brks}\n\nWriting config: ${sourceDir}/monitorSync.cfg";
-	echo "ALGORAND_DATA=${ALGORAND_DATA}" > ${sourceDir}/monitorConfig.cfg; echo -e "\nDone.\n";
+	echo "ALGORAND_DATA=${ALGORAND_DATA}" > ${configDir}/monitorConfig.cfg; echo -e "\nDone.\n";
 else
-source ${sourceDir}/monitorConfig.cfg;
+source ${configDir}/monitorConfig.cfg;
 fi;
 
 # Configuration - Report
@@ -53,8 +55,8 @@ diskMonitor=$(du ${ALGORAND_DATA} --summarize | awk '{print $1}');
 syncMonitor=$(grep "Catchpoint accounts processed:\|Catchpoint downloaded blocks:\|Catchpoint accounts verified:\|Round for next consensus protocol:" ${sourceDir}/lastStatus | tail -n 1 | awk '{print $NF}');
 lastBlock=$(grep "Last committed block:" ${sourceDir}/lastStatus | awk '{print $NF}');
 syncTime=$(grep "Sync Time:" ${sourceDir}/lastStatus | awk '{print $NF}')
-echo -e "${currentTime} ${diskMonitor} ${lastBlock} ${syncMonitor} ${syncTime}" >> ${sourceDir}/monitorSync.log;
+echo -e "${currentTime} ${diskMonitor} ${lastBlock} ${syncMonitor} ${syncTime}" >> ${logDir}/monitorSync.log;
 
 # Synchronization - Report
 echo -e "\n${brk}\nNode Synchronization Report\n${brk}\n";
-echo -e "Date Time Disk_Util Last_Block Round Sync\n$(head -n 1 ${sourceDir}/monitorSync.log)\n$(tail -n 17 ${sourceDir}/monitorSync.log)" | column -t
+echo -e "Date Time Disk_Util Last_Block Round Sync\n$(head -n 1 ${logDir}/monitorSync.log)\n$(tail -n 17 ${logDir}/monitorSync.log)" | column -t
