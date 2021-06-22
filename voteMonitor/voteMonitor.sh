5450cf96e7b1158b1fe7fa4007425304b2e06810
consiglieri-cfi <30444483+consiglieri-cfi@users.noreply.github.com>
2021-06-22 02:03:20 -0500

    Update voteMonitor.sh

--- voteMonitor/voteMonitor.sh
+++ voteMonitor/voteMonitor.sh
@@ -8,7 +8,10 @@
 #
 
 # Initialization
+nodeDir=~/node;
 sourceDir=$(dirname "$0");
+logDir=${sourceDir}/../logs;
+configDir=${sourceDir}/../config;
 currentDate=$(date +%Y-%m-%d);
 currentSecond=$(date +%H:%M:%S);
 currentEpoch=$(date +%s);
@@ -17,9 +20,9 @@ brk=$(printf '=%.0s' {1..120}); brks=$(printf '=%.0s' {1..30});
 echo -e "\n\n${brk}\nalgodMon - voteMonitor - Consensus Vote Monitor - Initialization\n${brk}";
 
 # Configuration - Part Key Address
-if [ -f ${sourceDir}/partWallet.src ]; then
-source ${sourceDir}/partWallet.src;
-echo -e "\n\nLoaded configuration: ${sourceDir}/partWallet.src\n\nParticipation Wallet: ${participationWallet}\n";
+if [ -f ${configDir}/partWallet.cfg ]; then
+source ${configDir}/partWallet.cfg;
+echo -e "\n\nLoaded configuration: ${configDir}/partWallet.cfg\n\nParticipation Wallet: ${participationWallet}\n";
 else
 echo -e "\n\nPlease enter the address for your participation key...\n"
 read participationWallet;
@@ -27,7 +30,7 @@ echo -e "\n\nYou have entered the following account..\n\n\t${participationWallet
 echo -e "\n\nIs this value correct? (y/n)\n\n"; read validateKey;
 if [ $validateKey = "y" ]; then
 echo -e "\n\nWriting configuration file..."
-echo -e "participationWallet=${participationWallet}" > ${sourceDir}/partWallet.src
+echo -e "participationWallet=${participationWallet}" > ${configDir}/partWallet.cfg
 echo -e "\n\nDone."
 else
 echo -e "\n\nPlease run again to retry key entry.\n\n"
@@ -37,7 +40,7 @@ fi; fi;
 # Vote Count - Previous Total
 if [ ! -f ${sourceDir}/pastVote.src ]; then
 echo -e "\n\nVote Count: No previous total found. File '${sourceDir}/pastVote.src' does not yet exist.\n"
-pastVotes=0
+pastVotes=0; dailyVotes=0; totalVotes=0;
 else
 source ${sourceDir}/pastVote.src;
 echo -e "\n\nLoaded configuration: ${sourceDir}/pastVote.src\n\nPrevious vote count: ${pastVotes}\n"
@@ -49,15 +52,15 @@ echo -e "\n\nLast Executed: $(date -r ${sourceDir}/pastVote.src +"%Y-%m-%d %H:%M
 # Count - Update
 currentVotes=$(grep -a VoteBroadcast ~/node/data/node.log | grep ${participationWallet} | wc -l); 
 totalVotes=$(expr ${pastVotes} + ${currentVotes});
-dailyVote=$(expr $(grep ${currentDate} ${sourceDir}/totalVote.log | awk '{sum+=$4}END{print sum}') + ${currentVotes})
+dailyVotes=$(expr $(grep ${currentDate} ${logDir}/totalVote.log | awk '{sum+=$4}END{print sum}') + ${currentVotes})
 
 # Count - Write
 echo -e "pastVotes=${totalVotes}" > ${sourceDir}/pastVote.src
-echo -e "${currentTime} \t ${pastVotes} \t ${currentVotes} \t ${dailyVote} \t ${totalVotes}" >> ${sourceDir}/totalVote.log
+echo -e "${currentTime} \t ${pastVotes} \t ${currentVotes} \t ${dailyVotes} \t ${totalVotes}" >> ${logDir}/totalVote.log
 
 # Count - Report
 echo -e "\n\n${brk}\nalgodMon - voteMonitor - Consensus Vote Monitor - Report\n${brk}\n";
-echo -e "Date Time Previous Current Today Total\n$(tail -n 20 ${sourceDir}/totalVote.log)" | column -t
+echo -e "Date Time Previous Current Today Total\n$(tail -n 20 ${logDir}/totalVote.log)" | column -t
 
 # Call Error Monitor
 ${sourceDir}/../errorMonitor/errorMonitor.sh;
