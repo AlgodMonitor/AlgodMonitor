053efd46fe65b37a5472703bb17bb292de08cd83
consiglieri-cfi <30444483+consiglieri-cfi@users.noreply.github.com>
2021-06-22 05:31:26 -0500

    Update storageMonitor.sh

--- storageMonitor/storageMonitor.sh
+++ storageMonitor/storageMonitor.sh
@@ -20,15 +20,15 @@ echo -e "\n\nLast Executed: $(date -r ${logDir}/monitorStorage.log +"%Y-%m-%d %H
 # Count - Update
 networkDir=${dataDir}/*net*;
 storageLog=${logDir}/monitorStorage.log;
-diskUtilization=$(du -xL ${dataDir}/../ --max-depth=0 | awk '{print $1}');
-sizeBlock=$(ls -l ${dataDir}/ledger.block.sqlite | awk '{print $5}');
-sizeBlockSHM=$(ls -l ${dataDir}/ledger.block.sqlite-shm | awk '{print $5}');
-sizeBlockWAL=$(ls -l ${dataDir}/ledger.block.sqlite-wal | awk '{print $5}');
-sizeTracker=$(ls -l ${dataDir}/ledger.tracker.sqlite | awk '{print $5}');
-sizeTrackerSHM=$(ls -l ${dataDir}/ledger.tracker.sqlite-shm | awk '{print $5}');
-sizeTrackerWAL=$(ls -l ${dataDir}/ledger.tracker.sqlite-wal | awk '{print $5}');
-sizeNodeLog=$(du -xL ${dataDir}/../node.log | awk '{print $1}');
-sizeErrLogs=$(du ${dataDir}/../algod-err* | awk '{sum+=$1}END{print sum}');
+diskUtilization=$(du -xL ${networkDir}/../ --max-depth=0 | awk '{print $1}');
+sizeBlock=$(ls -l ${networkDir}/ledger.block.sqlite | awk '{print $5}');
+sizeBlockSHM=$(ls -l ${networkDir}/ledger.block.sqlite-shm | awk '{print $5}');
+sizeBlockWAL=$(ls -l ${networkDir}/ledger.block.sqlite-wal | awk '{print $5}');
+sizeTracker=$(ls -l ${networkDir}/ledger.tracker.sqlite | awk '{print $5}');
+sizeTrackerSHM=$(ls -l ${networkDir}/ledger.tracker.sqlite-shm | awk '{print $5}');
+sizeTrackerWAL=$(ls -l ${networkDir}/ledger.tracker.sqlite-wal | awk '{print $5}');
+sizeNodeLog=$(du -xL ${networkDir}/../node.log | awk '{print $1}');
+sizeErrLogs=$(du ${networkDir}/../algod-err* | awk '{sum+=$1}END{print sum}');
 
 # Count - Write
 echo -e "${currentTime} ${diskUtilization} ${sizeBlock} ${sizeBlockSHM} ${sizeBlockWAL} ${sizeTracker} ${sizeTrackerSHM} ${sizeTrackerWAL} ${sizeNodeLog} ${sizeErrLogs}" >> ${storageLog};
