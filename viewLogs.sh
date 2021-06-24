5997ac7d74288256836b139e4956305509f9b13b
consiglieri-cfi <30444483+consiglieri-cfi@users.noreply.github.com>
2021-06-24 14:50:02 -0500

    Update viewLogs.sh

--- viewLogs.sh
+++ viewLogs.sh
@@ -38,7 +38,7 @@ else
 echo -e "\n\n${brk}\nalgodMon - viewLogs - Log Report Viewer\n${brk}\n";
 echo -e "\nDisplay 'algodMonitor' reports and file information on screen.\n\nPlease specify a component when running the './viewLogs' command.\n";
 echo -e "\nExample:\n\t ./viewLogs.sh all \n\t ./viewLogs.sh errors \n\t ./viewLogs.sh partkey \n";
-echo -e "\nOptions:\n\t all \n\t errors \n\t partkey \n\t tokens \n\t votes \n\t storage \n\t sync\n\n";
+echo -e "\nOptions:\n\t all \n\t errors \n\t network \n\t partkey \n\t tokens \n\t votes \n\t storage \n\t sync\n\n";
 echo -e "\nEnter a component name...\n\n"; read componentName;
 echo -e "\n\n\nDisplaying ${componentName} report...\n";
 ${sourceDir}/viewLogs.sh ${componentName};
