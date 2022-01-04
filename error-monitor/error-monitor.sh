f2950864068a4a58dea867f11949d9306c52f5ab
AlgodMonitor <30444483+AlgodMonitor@users.noreply.github.com>
2022-01-04 11:17:02 -0600

    Update error-monitor.sh

--- error-monitor/error-monitor.sh
+++ error-monitor/error-monitor.sh
@@ -8,14 +8,18 @@
 # Initialization
 globalValues=$(dirname "$0")/../config/globalValues.cfg;
 if [ ! -f ${globalValues} ]; then
-echo -e "\n\nERROR: Missing configuration file!\n\nExpected Path: ${globalValues}\n\n";
-kill ${BASHPID}; else source ${globalValues}; fi;
+	echo -e "\n\nERROR: Missing configuration file!\n\nExpected Path: ${globalValues}\n\n";
+	kill ${BASHPID}; 
+else 
+	source ${globalValues}; 
+fi;
 
 # Banner
 echo -e "\n\n${brk}\nalgodMon - errorMonitor - Node Error Monitor - Initialization\n${brk}";
 
 # Execution Tracker
-echo -e "\nLast Executed: $(date -r ${logDir}/monitorErrors.log +"%Y-%m-%d %H:%M:%S" 2>/dev/null)\nCurrent Time:  ${currentDate} ${currentSecond}\n"
+lastExec=$(date -r ${logDir}/monitorErrors.log +"%Y-%m-%d %H:%M:%S" 2>/dev/null)
+echo -e "\nLast Executed: ${lastExec}\nCurrent Time:  ${currentDate} ${currentSecond}\n"
 
 # Error Monitor - Processing
 echo -e "\n\n${brk}\nalgodMon - errorMonitor - Node Error Monitor - Processing\n${brk}\n";
@@ -49,57 +53,59 @@ echo -e "\n\n\n${brk}\nalgodMon - errorMonitor - Node Error Monitor - Report\n${
 mv ${errorReport} ${errorReport}-${currentEpoch};
 echo -e "\n${brkm}\nError Report\n${brkm}";
 if [[ ! ${errorCount} -gt 0 ]]; then
-        echo -e "\nNo errors found in algod node log:  ${ALGORAND_DATA}/node.log\n";
+        echo -e "\nNo errors found in algod node log:  ${dataDir}/node.log\n";
 else
-echo -e "\nALERT: Errors detected in algod node log:  ${ALGORAND_DATA}/node.log\n\n"
-tail ${errorReport}-${currentEpoch};
-echo -e "\n\nPlease review messages: ${errorReport}-${currentEpoch}\n"
+	echo -e "\nALERT: Errors detected in algod node log:  ${dataDir}/node.log\n\n"
+	tail ${errorReport}-${currentEpoch};
+	echo -e "\n\nPlease review messages: ${errorReport}-${currentEpoch}\n"
 fi;
 
 # errorReport - Compare last report
 if [[ ! -f ${lastEpochError} ]]; then
-echo -e "\nPrevious error report not found.\n";
+	echo -e "\nPrevious error report not found.\n";
 else
-echo -e "\nProcessing:  Comparing last error report...";
-diff ${lastEpochError} ${errorReport}-${currentEpoch} > /dev/null 2>&1
-diffStatus=$(echo $?)
-if [[ ${diffStatus} == 0 ]]; then
-echo -e "\nCurrent error report is a duplicate of the previous file.\n\n\tCurrent Report: ${errorReport}-${currentEpoch}\n\tLast Report: ${lastEpochError}\n\n"
-echo -e "Removing duplicate report:  ${errorReport}-${currentEpoch}"
-rm -f ${errorReport}-${currentEpoch};
-dailyError=0;
-else
-echo -e "\nNew error messages have been reported.\n\n"
-dailyError=1;
-fi; fi;
+	echo -e "\nProcessing:  Comparing last error report...";
+	diff ${lastEpochError} ${errorReport}-${currentEpoch} > /dev/null 2>&1
+	diffStatus=$(echo $?)
+	if [[ ${diffStatus} == 0 ]]; then
+		echo -e "\nCurrent error report is a duplicate of the previous file.\n\n\tCurrent Report: ${errorReport}-${currentEpoch}\n\tLast Report: ${lastEpochError}\n\n"
+		echo -e "Removing duplicate report:  ${errorReport}-${currentEpoch}"
+		rm -f ${errorReport}-${currentEpoch};
+		dailyError=0;
+	else
+		echo -e "\nNew error messages have been reported.\n\n"
+		dailyError=1;
+	fi;
+fi;
 
 # errorReport - Warning Detected
 mv ${warnReport} ${warnReport}-${currentEpoch};
 echo -e "\n\n${brkm}\nWarning Report\n${brkm}";
 if [[ ! ${warnCount} -gt 0 ]]; then
-	echo -e "\nNo warnings found in algod log:  ${ALGORAND_DATA}/node.log\n";
+	echo -e "\nNo warnings found in algod log:  ${dataDir}/node.log\n";
 else
-echo -e "\nALERT: Warnings detected in algod node log:  ${ALGORAND_DATA}/node.log\n\n"
-tail ${warnReport}-${currentEpoch};
-echo -e "\n\nPlease review messages: ${warnReport}-${currentEpoch}\n"
+	echo -e "\nALERT: Warnings detected in algod node log:  ${dataDir}/node.log\n\n"
+	tail ${warnReport}-${currentEpoch};
+	echo -e "\n\nPlease review messages: ${warnReport}-${currentEpoch}\n"
 fi;
 
 # errorReport - Compare last report
 if [[ ! -f ${lastEpochWarn} ]]; then
-echo -e "\nPrevious error report not found.\n";
+	echo -e "\nPrevious error report not found.\n";
 else
-echo -e "\nProcessing:  Comparing last warning report...";
-diff ${lastEpochWarn} ${warnReport}-${currentEpoch} > /dev/null 2>&1
-diffStatus=$(echo $?)
-if [[ ${diffStatus} == 0 ]]; then
-echo -e "\nCurrent warning report is a duplicate of the previous file.\n\n\tCurrent Report: ${warnReport}-${currentEpoch}\n\tLast Report: ${lastEpochWarn}\n\n"
-echo -e "Removing duplicate report:  ${errorReport}-${currentEpoch}"
-rm -f ${warnReport}-${currentEpoch};
-dailyWarn=0;
-else
-echo -e "\nNew error messages have been reported.\n\n"
-dailyWarn=1;
-fi; fi;
+	echo -e "\nProcessing:  Comparing last warning report...";
+	diff ${lastEpochWarn} ${warnReport}-${currentEpoch} > /dev/null 2>&1
+	diffStatus=$(echo $?)
+	if [[ ${diffStatus} == 0 ]]; then
+		echo -e "\nCurrent warning report is a duplicate of the previous file.\n\n\tCurrent Report: ${warnReport}-${currentEpoch}\n\tLast Report: ${lastEpochWarn}\n\n"
+		echo -e "Removing duplicate report:  ${errorReport}-${currentEpoch}"
+		rm -f ${warnReport}-${currentEpoch};
+		dailyWarn=0;
+	else
+		echo -e "\nNew error messages have been reported.\n\n"
+		dailyWarn=1;
+	fi; 
+fi;
 
 # Error Monitor - Historical
 echo -e "\n\n${brk}\nalgodMon - errorMonitor - Node Error Monitor - History\n${brk}\n";
@@ -107,18 +113,19 @@ errorHistory=${logDir}/monitorErrors.log
 
 # Count - Update
 if [ -f ${errorHistory} ]; then
-if [ ${dailyError} -gt 0 ]; then
-dailyError=$(expr $(grep ${currentDate} ${errorHistory} | awk '{sum+=$3}END{print sum}') + ${errorCount});
-echo -e "dailyError=${dailyError}" > ${sourceDir}/lastCountError.src
-else
-source ${sourceDir}/lastCountError.src 2>/dev/null;
+	if [ ${dailyError} -gt 0 ]; then
+		dailyError=$(expr $(grep ${currentDate} ${errorHistory} | awk '{sum+=$3}END{print sum}') + ${errorCount});
+		echo -e "dailyError=${dailyError}" > ${sourceDir}/lastCountError.src
+	else
+		source ${sourceDir}/lastCountError.src 2>/dev/null;
+	fi;
+	if [ ${dailyWarn} -gt 0 ]; then
+		dailyWarn=$(expr $(grep ${currentDate} ${errorHistory} | awk '{sum+=$5}END{print sum}') + ${warnCount});
+		echo -e "dailyWarn=${dailyWarn}" > ${sourceDir}/lastCountWarn.src
+	else
+		source ${sourceDir}/lastCountWarn.src 2>/dev/null;
+	fi; 
 fi;
-if [ ${dailyWarn} -gt 0 ]; then
-dailyWarn=$(expr $(grep ${currentDate} ${errorHistory} | awk '{sum+=$5}END{print sum}') + ${warnCount});
-echo -e "dailyWarn=${dailyWarn}" > ${sourceDir}/lastCountWarn.src
-else
-source ${sourceDir}/lastCountWarn.src 2>/dev/null;
-fi; fi;
 
 # Truncate Node Log
 echo -e "\nTruncating node log...\n"
