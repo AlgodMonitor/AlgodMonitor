c8dc2d5fef2bf80340cc4754ad9b7a68f85711ac
AlgodMonitor <30444483+AlgodMonitor@users.noreply.github.com>
2022-01-08 12:47:01 -0600

    Update view-logs.sh

--- view-logs.sh
+++ view-logs.sh
@@ -34,14 +34,22 @@ elif [ "${componentName}" = "storage" ]; then
 viewStorage=1;
 elif [ "${componentName}" = "sync" ]; then
 viewSync=1;
+elif [ "${componentName}" = "perf" ]; then
+viewPerf=1;
 else
-echo -e "\n\n${brk}\nAlgod Monitor - View Logs - Log Report Viewer\n${brk}\n";
-echo -e "\nDisplay 'Algod Monitor' reports and file information on screen.\n\nYou can enter a component when running the './viewLogs' command.\n";
-echo -e "\nExample:\n\t ./view-logs.sh all \n\t ./view-logs.sh errors \n\t ./view-logs.sh partkey \n";
-echo -e "\nOptions:\n\t all \n\t errors \n\t network \n\t partkey \n\t tokens \n\t votes \n\t storage \n\t sync\n\n";
+echo -e "\n\n${brk}\nalgodMon - viewLogs - Log Report Viewer\n${brk}\n";
+echo -e "\nDisplay 'algodMonitor' reports and file information on screen.\n\nPlease specify a component when running the 'viewLogs' command.\n";
+echo -e "\nExample:\n\t ./viewLogs.sh all \n\t ./viewLogs.sh errors \n\t ./viewLogs.sh partkey \n";
+echo -e "\nOptions:\n\t all \n\t errors \n\t network \n\t partkey \n\t tokens \n\t votes \n\t storage \n\t sync \n\t perf\n\n";
 echo -e "\nEnter a component name...\n\n"; read componentName;
 echo -e "\n\n\nDisplaying ${componentName} report...\n";
 ${sourceDir}/view-logs.sh ${componentName};
+fi;
+
+# Redirect - Config
+if [ "${viewPerf}" = "1" ]; then
+echo -e "\nExecuting: ${sourceDir}/perf-monitor/perf-monitor.sh\n";
+${sourceDir}/perf-monitor/perf-monitor.sh;
 fi;
 
 # View All
@@ -58,42 +66,49 @@ fi;
 
 # View Errors
 if [ ${viewErrors} == "1" ]; then
-echo -e "\n\n${brk}\nAlgod Monitor - Error Monitor - Node Error Messages\n${brk}\n";
-echo -e "Date Time Error Warning Err_Total Warn_Total Truncate SizeNew SizeOld FS_Type\n$(timeout 10 tail -n 25 ${logDir}/monitorErrors.log)" | column -t;
+echo -e "\n\n${brk}\nAlgod Monitor - Error Monitor - Node Error Report\n${brk}\n";
+echo -e "Date Time Error Warning Err_Total Warn_Total Truncate SizeNew SizeOld FS_Type\n$(timeout 10 tail -n 20 ${logDir}/monitorErrors.log)" | column -t;
 fi;
 
 # View Network
 if [ ${viewNetwork} == "1" ]; then
 echo -e "\n\n${brk}\nAlgod Monitor - Network Monitor - Network Peers Report\n${brk}\n";
-tail -n 30 $(ls -1tr ${logDir}/networkPeers/monitorPeers-* | tail -n1);
+firstReport=$(ls -1t ${logDir}/networkPeers/* | tail -n1)
+lastReport=$(ls -1tr ${logDir}/networkPeers/monitorPeers-* | tail -n1);
+tail -n 30 ${lastReport};
+echo -e "\n\nFirst Report: $(date -r ${firstReport} +"%Y-%m-%d %H:%M:%S")"
+echo -e "Last Report: $(date -r ${lastReport} +"%Y-%m-%d %H:%M:%S")"
+
+echo -e "\n\nTotal Peers: $(wc -l ${lastReport} | awk '{print $1}')"
+echo -e "Total Records: $(awk '{sum+=$1}END{print sum}' ${lastReport})\n\n"
 fi;
 
 # View Part Key
 if [ ${viewPartKey} == "1" ]; then
-echo -e "\n\n${brk}\nAlgod Monitor - Part Key Monitor - Key Expiration Timer\n${brk}\n";
-echo -e "Date Time Current End RoundsLeft DaysLeft Expiration Registered\n$(timeout 10 tail -n 25 ${logDir}/monitorExpiration.log)" | column -t
+echo -e "\n\n${brk}\nAlgod Monitor - Part Ley Monitor - Participation Key Expiration Report\n${brk}\n";
+echo -e "Date Time Current End RoundsLeft DaysLeft Expiration Registered\n$(timeout 10 tail -n 20 ${logDir}/monitorExpiration.log)" | column -t
 fi;
 
 # View Tokens
 if [ ${viewTokens} == "1" ]; then
-echo -e "\n\n${brk}\nAlgod Monitor - Token Monitor - Account Balance Monitor\n${brk}\n";
-echo -e "New report coming soon..."
+echo -e "\n\n${brk}\nAlgod Monitor - Token Monitor - Account Balance Report\n${brk}\n";
+echo -e "Report coming soon...";
 fi;
 
 # View Votes
 if [ ${viewVotes} == "1" ]; then
-echo -e "\n\n${brk}\nAlgod Monitor - Vote Monitor - Consensus Vote Tracker\n${brk}\n";
-echo -e "Date Time Previous New Today Total\n$(timeout 10 tail -n 25 ${logDir}/monitorVotes.log)" | column -t
+echo -e "\n\n${brk}\nAlgod Monitor - Vote Monitor - Consensus Vote Report\n${brk}\n";
+echo -e "Date Time Previous New Today Total\n$(timeout 10 tail -n 20 ${logDir}/monitorVotes.log)" | column -t
 fi;
 
 # View Storage
 if [ ${viewStorage} == "1" ]; then
 echo -e "\n\n${brk}\nAlgod Monitor - Storage Monitor - Storage Utilization Report\n${brk}\n";
-echo -e "Date Time Utilized block.db b-shm b-wal tracker.db t-shm t-wal node.log algod-err\n$(timeout 10 tail -n 25 ${logDir}/monitorStorage.log)" | column -t;
+echo -e "Date Time Utilized block.db b-shm b-wal tracker.db t-shm t-wal node.log algod-err\n$(timeout 10 tail -n 20 ${logDir}/monitorStorage.log)" | column -t;
 fi;
 
 # View Sync
 if [ ${viewSync} == "1" ]; then
-echo -e "\n\n${brk}\nAlgod Monitor - Sync Monitor - Node Synchronization Monitor\n${brk}\n";
-echo -e "Date Time Disk_Util Last_Block Round Sync\n$(timeout 10 tail -n 25 ${logDir}/monitorSync.log)" | column -t
+echo -e "\n\n${brk}\nAlgod Monitor - Sync Monitor - Node Synchronization Report\n${brk}\n";
+echo -e "Date Time Disk_Util Last_Block Round Sync\n$(timeout 10 tail -n 20 ${logDir}/monitorSync.log)" | column -t
 fi;
