bf9d7126b315f10a357ff22207e0598f73f62e35
AlgodMonitor <30444483+AlgodMonitor@users.noreply.github.com>
2022-02-05 16:27:23 -0600

    Update batch-monitor.sh

--- batch-monitor.sh
+++ batch-monitor.sh
@@ -9,6 +9,6 @@ sourceDir=$(dirname "$0");
 ${sourceDir}/network-monitor/network-monitor.sh
 ${sourceDir}/partkey-monitor/partkey-monitor.sh
 ${sourceDir}/storage-monitor/storage-monitor.sh
-${sourceDir}/token-monitor/token-monitor.sh
+# ${sourceDir}/token-monitor/token-monitor.sh
 ${sourceDir}/sync-monitor/sync-monitor.sh
 ${sourceDir}/vote-monitor/vote-monitor.sh
